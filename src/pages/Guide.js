e070895cc60504eac8316a82a07cfed98cf2bb82
DevBaddy <warbaddy19@gmail.com>
2022-01-23 19:08:59 -0400

    Updated the guide

--- src/pages/Guide.js
+++ src/pages/Guide.js
@@ -11,36 +11,39 @@ const Guide = () => {
       <div className="container d-flex flex-column align-items-center text-left pt-4 pb-4">
         <div>
           <h1>A Guide To Using Bet Royale</h1>
-          <p className="pt-3">Note: This project is still in beta.</p>
-          <p className="pt-3">This is a guide on how to use Bet Royale.</p>
+          <p className="pt-3">
+            <strong>Note: This project is still in beta.</strong>
+          </p>
           <h2 className="pt-3">
             What Do You Need To Do Everything On This Website?
           </h2>
           <p className="pt-3">
             You need a Metamask wallet and a Discord account, that's all you
-            need. You need both your Metamask wallet <br /> and Discord account
-            in order to bet. In order to create bets, you just need your Discord
+            need. You need both your Metamask wallet and Discord account in
+            order to bet. In order to create bets, you just need your Discord
             account.
           </p>
           <h2 className="pt-3">The Home Page</h2>
           <p className="pt-3">
-            The Home Page is where you can see the current bets that are active.
+            The Home Page is where you can see the current bets.
           </p>
           <h2 className="pt-3">The Create Bet Page</h2>
           <p className="pt-3">
             The Create Bet page is where you create bets. You have to set
-            parameters such as: Name, Description <br /> Deadline, Results, etc.
+            parameters such as: Name, Description Deadline, Results, etc.
             Remember, when setting a deadline and result, you are setting them
-            in UTC time, <br /> so be aware of everyone's timezones when setting
-            those parameters.
+            in UTC time, so be aware of everyone's timezones when setting the
+            deadline and results parameters.
           </p>
           <h2 className="pt-3">The Betting History Page</h2>
           <p className="pt-3">
+            The Betting History Page is where you can see the history of all of
+            the bets that have been made on the website.
+          </p>
+          <h2 className="pt-3">The My Betting History Page</h2>
+          <p className="pt-3">
             The Betting History Page is where you can see your history of bets
             you have betted on.
-          </p>
-          <p className="pt-3">
-            That's all you need to know when using this website!
           </p>
         </div>
       </div>
